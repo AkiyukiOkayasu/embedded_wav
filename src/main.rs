c736dc5c55d9e64edf756c41525d03d601dc0ccd
Akiyuki Okayasu <akiyuki@tkrworks.net>
2022-10-24 17:35:48 +0900

    不要になった変数を削除

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod reader;
 
 fn main() {
     let wav = include_bytes!("../resources/test.wav");
-    let file_length = wav.len();
     println!("Wave length in bytes: {}", wav.len());
 
     let reader = reader::PcmReader::read_bytes(wav).unwrap();
