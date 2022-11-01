799b7975af26b9ed6ba9bd68d7abaf9a3f20e7da
Akiyuki Okayasu <akiyuki@tkrworks.net>
2022-11-01 22:38:05 +0900

    cpal経由で再生した

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
 mod reader;
 
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
 fn main() {
     let wav = include_bytes!("../resources/test.wav");
     println!("Wave length in bytes: {}", wav.len());
 
+    let host = cpal::default_host();
+    let device = host.default_output_device().unwrap();
+    println!("Default output device: {:?}", device.name());
+
+    let config = device.default_output_config().unwrap();
+    println!("Default output config: {:?}", config);
+    let channels = config.channels() as usize;
+
     let reader = reader::PcmReader::read_bytes(wav);
-    for i in 0..=100 {
-        println!("{}", reader.read_sample(0, i).unwrap());
-    }
+    let mut sample_index = 0;
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+    let stream = device
+        .build_output_stream(
+            &config.into(),
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                // write_data(data, channels, &mut next_value)
+                for frame in data.chunks_mut(channels) {
+                    for sample in frame.iter_mut() {
+                        *sample = reader.read_sample(0, sample_index).unwrap();
+                    }
+                    sample_index += 1;
+                }
+            },
+            err_fn,
+        )
+        .unwrap();
+    stream.play().unwrap();
+
+    std::thread::sleep(std::time::Duration::from_millis(1000));
 }
